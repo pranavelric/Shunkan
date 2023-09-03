ab47966e53e8b6a4eab40f57b7c28ae6223fbc6d
Pranav Choudhary <pranavchoudhary500@gmail.com>
2023-09-03 19:52:01 +0530

    Initial Commit

---
+++ Shunkan/ShunkanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShunkanApp.swift
+//  Shunkan
+//
+//  Created by Pranav Choudhary on 03/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ShunkanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
